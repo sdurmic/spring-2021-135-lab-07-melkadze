fdb1d722d19193661fb761c1ac196964b796b002
Nick Melkadze <25963683+melkadze@users.noreply.github.com>
2021-03-14 23:08:45 -0400

    format main

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 #include "funcs.h"
 
 int main() {
-    // read the entire cin input until eos/eof
-    // instead of per line or char by using istreambuf_iterator
+    // read the entire cin input until eos/eof instead of
+    // per line or char by using istreambuf_iterator
     std::string input(std::istreambuf_iterator<char>(std::cin), {});
+    
+    // perform both tasks through one helper function, and print
     std::cout << style(input);
     
     return 0;
