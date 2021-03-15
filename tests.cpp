c26a7a4c0a0dc3990a88ec795aba6b173ba0bab0
Nick Melkadze <25963683+melkadze@users.noreply.github.com>
2021-03-14 22:10:10 -0400

    finish A tests

--- tests.cpp
+++ tests.cpp
@@ -1,3 +1,15 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "funcs.h"
+
+TEST_CASE("[A: removeLeadingSpaces] Do nothing") {
+    CHECK(removeLeadingSpaces("no_spaces") == "no_spaces");
+    CHECK(removeLeadingSpaces("spaces, none of which are leading or trailing") == "spaces, none of which are leading or trailing");
+    CHECK(removeLeadingSpaces("no leading spaces, but normal and trailing spaces   ") == "no leading spaces, but normal and trailing spaces   ");
+}
+
+TEST_CASE("[A: removeLeadingSpaces] Remove spaces") {
+    CHECK(removeLeadingSpaces("    leading_spaces_only") == "leading_spaces_only");
+    CHECK(removeLeadingSpaces("         leading spaces and spaces") == "leading spaces and spaces");
+    CHECK(removeLeadingSpaces("    leading and trailing spaces   ") == "leading and trailing spaces   ");
+}
